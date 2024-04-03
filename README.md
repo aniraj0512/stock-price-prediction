941d00d89072f1a22b50c71f1ad921af72c4a0db
Rohit Bhure <rohitbhure.cse@gmail.com>
2024-04-03 21:12:06 +0530

    project completed

---
+++ README.md
@@ -0,0 +1,55 @@
+
+# Stock Price Prediction Using Machine Learning Stacked LSTM Model Project Version 1.0
+
+Welcome to the Stock Price Prediction project repository! This project utilizes machine learning techniques to predict stock prices based on historical data. Predicting stock prices is a challenging task, and this project aims to explore different machine learning models to forecast future stock prices.
+
+
+# Key Features:
+
+- Data Collection: Gathered historical stock price data from reliable sources to build a comprehensive dataset.
+- Exploratory Data Analysis (EDA): Conducted thorough analysis to understand patterns, trends, and relationships within the stock data.
+- Feature Engineering: Extracted relevant features and engineered new ones to improve the model's predictive performance.
+- Machine Learning Models: Implemented and compared various machine learning models, including but not limited to LSTM (Long Short-Term Memory) networks, to predict stock prices.
+- Evaluation Metrics: Utilized appropriate metrics to evaluate and compare the performance of the models.
+- Visualization: Created insightful visualizations to better understand the predictions and model performance.
+
+## Technologies Used:
+
+- Python
+- TensorFlow
+- Scikit-Learn
+- Pandas
+- Matplotlib
+- Jupyter Notebooks
+
+## Getting Started:
+
+Clone the project
+
+```bash
+  git clone https://github.com/rohitbhure65/stock-price-prediction-using-machine-learning
+```
+
+Go to the project directory
+
+```bash
+  cd stock-price-prediction-using-machine-learning
+```
+
+install important python Libraries
+
+```bash
+  pip3 install -r requirement.txt
+```
+
+Start the server
+
+```bash
+  streamlit run stock_price_prediction.py
+```
+
+Explore the Jupyter Notebooks in the notebooks directory for detailed analysis and model implementation.
+
+## License:
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ stock_price_prediction.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import load_model
+import streamlit as st
+import datetime as dt
+import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+
+st.title('Stock Market Trend Prediction')
+st.divider()
+st.sidebar.header('User Input')
+
+STOCK = st.sidebar.text_input("Enter Stock Ticker", 'ADANIPORTS.NS')
+start_date = st.sidebar.date_input("Enter Start Date", dt.date(2000,1,1))
+end_date = st.sidebar.date_input("Enter End Date", dt.datetime.now().date())
+
+df = yf.download(STOCK,start=start_date, end=end_date, progress=False)
+st.subheader('Stock '+STOCK+' ( '+str(pd.to_datetime(start_date).date())+' - '+str(pd.to_datetime(end_date).date())+' )')
+
+st.table(df.describe())
+
+st.header('Closing Price VS Time Chart')
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+plt.legend(['Closing Price'])
+plt.title("Closing Price Vs Time Chart")
+plt.grid()
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
+
+st.header("Closing price Vs Time Chart with 100 Days Moving Average")
+ma100 = df.Close.rolling(100).mean()
+fig = plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+plt.plot(ma100, 'r')
+plt.legend(['Closing Price','100 Days Moving Average'])
+plt.grid()
+plt.title("Closing Price Vs Time Chart with 100 Days Moving Average")
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
+
+st.header('Closing Price Vs Time Chart with 100 & 200 Days Moving Average')
+ma100=df.Close.rolling(100).mean()
+ma200=df.Close.rolling(200).mean()
+fig=plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+plt.plot(ma100, 'r')
+plt.plot(ma200, 'g')
+plt.legend(['Closing price', '100 Days Moving Average', '200 Days Moving Average'])
+plt.grid()
+plt.title("Closing Price Vs Time Chart with 100 & 200 Days Moving Average")
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig)
+
+data_training = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
+data_testing = pd.DataFrame(df['Close'][int(len(df)*0.70): int(len(df))])
+
+
+scaler = MinMaxScaler(feature_range=(0,1))
+data_training_array = scaler.fit_transform(data_training)
+
+model = load_model('keras_model.h5')
+
+past_100_days = data_training.tail(100)
+final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+input_data = scaler.fit_transform(final_df)
+
+x_test = []
+y_test = []
+for i in range(100,input_data.shape[0]):
+    x_test.append(input_data[i-100:i])
+    y_test.append(input_data[i,0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_predicted = model.predict(x_test)
+scaler =scaler.scale_
+
+scale_factor = 1/scaler[0]
+y_predicted = y_predicted * scale_factor
+y_test = y_test * scale_factor
+
+st.header('Predection Vs Original')
+fig2 = plt.figure(figsize=(12,6))
+plt.plot(y_test, 'b')
+plt.plot(y_predicted, 'r')
+plt.legend(['Original Price', 'Predicted Price'])
+plt.grid()
+plt.title("Predection Vs Original")
+plt.xlabel('Time')
+plt.ylabel('Price')
+st.pyplot(fig2)
